94143730415b2e5910a3416a9ff72ddb61b282ff
drbarazi <muhammad.agessy.barazi@gmail.com>
2024-08-01 17:10:59 +0700

    setup dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		'react-native-reanimated/plugin',
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+				alias: {
+					tests: ['./tests/'],
+					"@navigation": "./src/navigation",
+					"@screens": "./src/screens",
+					"@components": "./src/components",
+					"@theme": "./src/theme/index",
+					'@api': './src/api',
+					'@assets': './src/assets',
+					'@redux': './src/redux',
+				}
+			}
+		]
+	],
 };
